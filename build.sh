d83afa95a7ef249c080f4780d3ecafafca11d589
RamaAlqdri <ramadhanialqadri12@gmail.com>
2024-12-06 22:43:46 +0800

    update

--- build.sh
+++ build.sh
@@ -5,7 +5,10 @@ apt-get update && apt-get install -y \
     python3 \
     python3-pip \
     libgl1-mesa-glx \
-    libglib2.0-0
+    libglib2.0-0 \
+    libsm6 \
+    libxrender1 \
+    libxext6
 
 # Instal dependensi Python
 pip3 install --no-cache-dir -r requirements.txt
