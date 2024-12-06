08cdf9f5338819836cb9f0be7741c9b304b000be
RamaAlqdri <ramadhanialqadri12@gmail.com>
2024-12-06 22:52:16 +0800

    update

--- build.sh
+++ build.sh
@@ -1,5 +1,20 @@
 #!/bin/bash
+# Instal GLIBC versi terbaru
+GLIBC_VERSION="2.38"
 
+# Unduh dan instal GLIBC
+apt-get update && apt-get install -y build-essential manpages-dev wget
+wget http://ftp.gnu.org/gnu/libc/glibc-$GLIBC_VERSION.tar.gz
+tar -xvf glibc-$GLIBC_VERSION.tar.gz
+cd glibc-$GLIBC_VERSION
+mkdir build
+cd build
+../configure --prefix=/opt/glibc-$GLIBC_VERSION
+make -j$(nproc)
+make install
+
+# Tambahkan GLIBC baru ke library path
+export LD_LIBRARY_PATH=/opt/glibc-$GLIBC_VERSION/lib:$LD_LIBRARY_PATH
 # Perbarui repository dan install library sistem yang diperlukan
 apt-get update && apt-get install -y \
     python3 \
